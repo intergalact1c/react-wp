a944e2ee431f0ce83e0947b6b7e1b642ca7d1c40
intergalact1c <64426764+intergalact1c@users.noreply.github.com>
2021-02-21 11:21:50 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         fieldName: "wordpress",
         // Url to query from
         //url: "http://react-wp.local/wp/graphql",
-        url: "http://cr14494-wordpress.tw1.ru/graphql",
+        url: "http://react-wp.ru/graphql",
 
         // refetch interval in seconds
         //refetchInterval: 60,
